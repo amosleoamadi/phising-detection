281c6421d3ba97fc3a28a05db72b41c76377906e
amosleoamadi <amadiamos146@gmail.com>
2026-05-13 15:29:21 +0100

    feat: added the CORS

--- app/main.py
+++ app/main.py
@@ -1,7 +1,20 @@
 from fastapi import FastAPI
 from app.routes.predictor import router 
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(title="Phishing Detection API")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://localhost:3000",
+        "http://127.0.0.1:8000",
+    ],
+    allow_origin_regex=r"http://(localhost|127\.0\.0\.1):\d+",
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(router)
 
